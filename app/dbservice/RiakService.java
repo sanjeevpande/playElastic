c9e59d32a8e782e1556825237c5e0194987b73fc
Kavya <kavya.bavikatti@getzephyr.com>
2016-04-15 09:50:40 +0000

    hgvwadhbdvb

---
+++ app/dbservice/RiakService.java
@@ -0,0 +1,7 @@
+package dbservice;
+
+/**
+ * Created by Kavya on 23-08-2015.
+ */
+public class RiakService {
+}
---
+++ app/dbservice/impl/RiakServiceImpl.java
@@ -0,0 +1,7 @@
+package dbservice.impl;
+
+/**
+ * Created by Kavya on 23-08-2015.
+ */
+public class RiakServiceImpl {
+}
